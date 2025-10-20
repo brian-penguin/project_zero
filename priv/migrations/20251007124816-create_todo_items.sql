02ab1fc20e4ddee57dba3105ce259fe257634cb3
brimatteng <btenggren20@gmail.com>
2025-10-20 16:24:22 -0400

    Swap to timestamp from timstamptz
    Why this change is being made:
    - POG which is the gleam lib built on pgo doesn't currently support timstamptz
    - timstamptz is a column which will convert the inserted data to UTC
      regardless of which timezone is contained in the timstamp we are
      inserting. This means we will always store in UTC without having to
      manually manage.
      - Since this doesn't work we will have to convert user input to the
        utc and then back to the user timezone on read
    
    What were the changes made to support this:
    - rollback and change the timestamp

--- priv/migrations/20251007124816-create_todo_items.sql
+++ priv/migrations/20251007124816-create_todo_items.sql
@@ -1,23 +1,21 @@
 --- migration:up
-BEGIN;
 CREATE TABLE IF NOT EXISTS todo_items (
     id uuid PRIMARY KEY DEFAULT uuidv7 (),
     title text NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT NOW(),
-    updated_at timestamptz NOT NULL DEFAULT NOW(),
-    completed_at timestamptz
+-- in a dream world these would be timstamptz to automatically handle the utc -> local time storage
+--  BUT the POG library doesn't support that as of yet. We will have to hand manage time
+    created_at timestamp NOT NULL DEFAULT NOW(),
+    updated_at timestamp NOT NULL DEFAULT NOW(),
+    completed_at timestamp
 );
 CREATE TRIGGER update_todo_items_updated_at
     BEFORE UPDATE ON todo_items
     FOR EACH ROW
     EXECUTE FUNCTION update_updated_at_column ();
 CREATE INDEX idx_todo_items_created_at ON todo_items (created_at);
-COMMIT;
 
 --- migration:down
-BEGIN;
-DROP INDEX CONCURRENTLY IF EXISTS idx_todo_items_created_at;
+DROP INDEX IF EXISTS idx_todo_items_created_at;
 DROP TABLE IF EXISTS todo_items;
-COMMIT;
 
 --- migration:end
